d08a5d34968bed8bf3a0039b2b504956c719a13b
川路慎一 <bowmoq@gmail.com>
2019-08-22 07:22:06 +0900

    ch04 練習問題3

--- ch04/exercise.hs
+++ ch04/exercise.hs
@@ -17,3 +17,21 @@ thirdB xs = xs !! 2
 -- パターンマッチ
 thirdC :: [a] -> a
 thirdC (_:_:x:xs) = x
+
+-- 練習問題3
+-- 空リストのときは空リストを返す安全なsafetailの実装
+-- safetail :: [a] -> [a]
+-- tailは先頭の要素を覗いたリストを返す
+-- 条件式を使って
+safetailA :: [a] -> [a]
+safetailA xs = if null xs then [] else tail xs
+
+-- ガードを使って
+safetailB :: [a] -> [a]
+safetailB xs | null xs = []
+             | otherwise = tail xs
+
+-- パターンマッチ
+safetailC :: [a] -> [a]
+safetailC [] = []
+safetailC (x:xs) = xs
