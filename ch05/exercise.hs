5f3d54ca7da6eae26298ddb38b17b742252ebff2
川路慎一 <bowmoq@gmail.com>
2019-08-23 21:04:24 +0900

    exercise 5.8

--- ch05/exercise.hs
+++ ch05/exercise.hs
@@ -23,3 +23,15 @@ factors n = [x | x <- [1..n], n `mod` x == 0]
 
 perfects :: Int -> [Int]
 perfects n = [x | x <- [1..n], sum (factors x) == x * 2]
+
+-- 5.7
+
+-- 5.8
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [i | (x', i) <- zip xs [0..], x == x']
+
+find :: Eq a => a -> [(a, b)] -> [b]
+find k t = [v | (k', v) <- t, k == k']
+
+positions' :: Eq a => a -> [a] -> [Int]
+positions' x xs = find x $ zip xs [0..]
