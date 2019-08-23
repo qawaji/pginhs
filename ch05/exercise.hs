4371491c586da9f8d15885dac47a6a8073ce068e
川路慎一 <bowmoq@gmail.com>
2019-08-23 13:06:15 +0900

    exercise 5.6

--- ch05/exercise.hs
+++ ch05/exercise.hs
@@ -16,3 +16,10 @@ myReplicate n a = [a| _ <- [1..n]]
 -- 5.5
 pyths :: Int -> [(Int, Int, Int)]
 pyths n = [(x, y, z)| x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
+
+-- 5.6
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], sum (factors x) == x * 2]
