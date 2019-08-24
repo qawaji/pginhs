0bd05ed764bc20b86c1859cbd596c42f9c8fc110
川路慎一 <bowmoq@gmail.com>
2019-08-25 08:45:45 +0900

    ch07 exercise 7.1, 7.2

---
+++ ch07/exercise.hs
@@ -0,0 +1,20 @@
+-- 7.1
+-- [f x | x <- xs, p x] を map, filterをつかって
+myfunc f p xs = map f (filter p xs)
+
+-- 7.2
+all' :: (a -> Bool) -> [a] -> Bool
+all' p xs = and $ map p xs
+
+any' :: (a -> Bool) -> [a] -> Bool
+any' p xs = or $ map p xs
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' p (x:xs) | p x = x : takeWhile' p xs
+                    | otherwise = []
+
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' _ [] = []
+dropWhile' p (x:xs) | p x = x:xs
+                    | otherwise = dropWhile' p xs
