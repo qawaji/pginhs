a973103f647c48fa9bbaffa8fa3071f3ac53e955
川路慎一 <bowmoq@gmail.com>
2019-08-26 07:09:43 +0900

    unfoldでiterate f

--- ch07/exercise.hs
+++ ch07/exercise.hs
@@ -60,6 +60,8 @@ chop8 bits = take 8 bits : chop8 (drop 8 bits)
 chop8' :: [Bit] -> [[Bit]]
 chop8' = unfold null (take 8) (drop 8)
 
+iterate' f = unfold (\_ -> False) id f
+
 -- 7.9 altMap
 altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
 altMap _ _ [] = []
