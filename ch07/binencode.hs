04a272463f8b9485eed6c34ace06ed6aba30e984
川路慎一 <bowmoq@gmail.com>
2019-08-26 16:18:37 +0900

    exercise 7.7, 7.8をbinencode.hsに追加

--- ch07/binencode.hs
+++ ch07/binencode.hs
@@ -1,5 +1,7 @@
 import Data.Char
 
+-- 7.8, 7.9
+
 type Bit = Int
 
 bin2int :: [Bit] -> Int
@@ -15,17 +17,42 @@ make8 :: [Bit] -> [Bit]
 make8 bits = take 8 (bits ++ repeat 0)
 
 encode :: String -> [Bit]
-encode = concat . map (make8 . int2bin . ord)
+encode = concat . map (addParityBit . make8 . int2bin . ord)
+
+chop :: Int -> [Bit] -> [[Bit]]
+chop _ [] = []
+chop n bits = take n bits : chop n (drop n bits)
 
 chop8 :: [Bit] -> [[Bit]]
-chop8 [] = []
-chop8 bits = take 8 bits : chop8 (drop 8 bits)
+chop8 = chop 8
+
+chop9 :: [Bit] -> [[Bit]]
+chop9 = chop 9
 
 decode :: [Bit] -> String
-decode = map (chr . bin2int) . chop8
+decode = map (chr . bin2int . getBits) . chop9
+  where
+    getBits bs | parityCheck bs = tail bs
+               | otherwise = error "error parity check"
 
 transmit :: String -> String
 transmit = decode . channel . encode
 
 channel :: [Bit] -> [Bit]
 channel = id
+
+err_channel :: [Bit] -> [Bit]
+err_channel = tail
+
+countBit :: [Bit] -> Int
+countBit = length . filter (\x -> x == 1)
+
+parityBit :: [Bit] -> Bit
+parityBit bs | odd $ countBit bs = 1
+             | otherwise = 0
+
+addParityBit :: [Bit] -> [Bit]
+addParityBit bs = (parityBit bs):bs
+
+parityCheck :: [Bit] -> Bool
+parityCheck bs = (parityBit $ tail bs) == head bs
