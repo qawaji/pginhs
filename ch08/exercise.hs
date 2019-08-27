d2ac1726a05225e3114976cff2bb56fdad21e726
川路慎一 <bowmoq@gmail.com>
2019-08-27 19:47:23 +0900

    exercise 8.2

--- ch08/exercise.hs
+++ ch08/exercise.hs
@@ -8,3 +8,13 @@ add (Succ m) n = Succ (add m n)
 mult :: Nat -> Nat -> Nat
 mult m Zero     = Zero
 mult m (Succ n) = add m (mult m n)
+
+-- 8.2
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
+occurs :: Ord a => a -> Tree a -> Bool
+occurs x (Leaf y) = x == y
+occurs x (Node l y r) case x `compare` y of
+                        LT -> occurs x l
+                        EQ -> True
+                        GT -> occurs x r
