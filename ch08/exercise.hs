592adad0fb555c19d6faeb423d57db329fd436f5
川路慎一 <bowmoq@gmail.com>
2019-08-27 20:11:07 +0900

    exercise 8.3

--- ch08/exercise.hs
+++ ch08/exercise.hs
@@ -14,7 +14,19 @@ data Tree a = Leaf a | Node (Tree a) a (Tree a)
 
 occurs :: Ord a => a -> Tree a -> Bool
 occurs x (Leaf y) = x == y
-occurs x (Node l y r) case x `compare` y of
-                        LT -> occurs x l
-                        EQ -> True
-                        GT -> occurs x r
+occurs x (Node l y r) = case x `compare` y of
+                          LT -> occurs x l
+                          EQ -> True
+                          GT -> occurs x r
+
+-- 8.3
+data Tree' a = Leaf' a | Node' (Tree' a) (Tree' a)
+
+countLeaf :: Tree' a -> Int
+countLeaf (Leaf' x) = 1
+countLeaf (Node' l r) = countLeaf l + countLeaf r
+
+balanced :: Tree' a -> Bool
+balanced (Leaf' x) = True
+balanced (Node' l r) = balanced l && balanced r && abs (countLeaf r - countLeaf l) <= 1
+
