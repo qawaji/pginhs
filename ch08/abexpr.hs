24aeb7f994e6aa11f06ac5a0b42c5040508f5b6e
川路慎一 <bowmoq@gmail.com>
2019-08-28 07:37:05 +0900

    exercise 8.9 乗算の追加

--- ch08/abexpr.hs
+++ ch08/abexpr.hs
@@ -1,25 +1,25 @@
 -- 抽象機械
-data Expr = Val Int | Add Expr Expr
-
-value :: Expr -> Int
-value (Val n) = n
-value (Add x y) = value x + value y
+-- exercise 8.9 乗算の追加
+data Expr = Val Int | Add Expr Expr | Mul Expr Expr
 
 type Cont = [Op]
 
-data Op = EVAL Expr | ADD Int
+data Op = EVAL Expr | ADD Int | MUL Int
 
 eval :: Expr -> Cont -> Int
 eval (Val n) c = exec c n
-eval (Add x y) c = eval x (EVAL y : c)
+eval (Add x y) c = eval x (EVAL y : ADD 0 : c)
+eval (Mul x y) c = eval x (EVAL y : MUL 1 : c)
 
 exec :: Cont -> Int -> Int
 exec [] n = n
-exec (EVAL y : c) n = eval y (ADD n : c)
+exec (EVAL y : ADD _ : c) n = eval y (ADD n : c)
+exec (EVAL y : MUL _ : c) n = eval y (MUL n : c)
 exec (ADD n : c) m = exec c (n+m)
+exec (MUL n : c) m = exec c (n*m)
 
-value' :: Expr -> Int
-value' e = eval e []
+value :: Expr -> Int
+value e = eval e []
 
 -- exercise 8.5
 folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
