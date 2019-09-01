ff598e2fa25669fb5db2432717efe7c31b38cea6
川路慎一 <bowmoq@gmail.com>
2019-09-02 08:47:51 +0900

    add ch11 tictactoe

---
+++ ch11/tictactoe.hs
@@ -0,0 +1,28 @@
+import Data.Char
+import Data.List
+import System.IO
+
+size :: Int
+size = 3
+
+type Grid = [[Player]]
+
+data Player = O | B | X
+              deriving (Eq, Ord, Show)
+
+next :: Player -> Player
+next O = X
+next B = B
+next X = O
+
+empty :: Grid
+empty = replicate size (replicate size B)
+
+full :: Grid -> Bool
+full = all (/= B) . concat
+
+turn :: Grid -> Player
+turn g = if os <= xs then O else X
+  where os = length (filter (== O) ps)
+        xs = length (filter (== X) ps)
+        ps = concat g
