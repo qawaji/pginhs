157a41be2fd547b48d42a7123c25d6aad38788dc
川路慎一 <bowmoq@gmail.com>
2019-09-02 23:04:41 +0900

    update tictactoe.hs

--- ch11/tictactoe.hs
+++ ch11/tictactoe.hs
@@ -26,3 +26,38 @@ turn g = if os <= xs then O else X
   where os = length (filter (== O) ps)
         xs = length (filter (== X) ps)
         ps = concat g
+
+wins :: Player -> Grid -> Bool
+wins p g = any line (rows ++ cols ++ dias)
+           where
+            line = all (== p)
+            rows = g
+            cols = transpose g
+            dias = [diag g, diag (map reverse g)]
+
+diag :: Grid -> [Player]
+diag g = [g !! n  !! n | n <- [0..size-1]]
+
+won :: Grid -> Bool
+won g = wins O g || wins X g
+
+putGrid :: Grid -> IO ()
+putGrid = 
+  putStrLn .unlines . concat . interleave bar . map showRow
+  where bar = [replicate((size*4)-1) '-']
+
+showRow :: [Player] -> [String]
+showRow = beside . interleave bar . map showPlayer
+          where
+            beside = foldr1 (zipWith (++))
+            bar = replicate 3 "|"
+
+showPlayer :: Player -> [String]
+showPlayer O = ["   ", " O ", "   "]
+showPlayer B = ["   ", "   ", "   "]
+showPlayer X = ["   ", " X ", "   "]
+
+interleave :: a -> [a] -> [a]
+interleave x [] = []
+interleave x [y] = [y]
+interleave x (y:ys) = y : x : interleave x ys
