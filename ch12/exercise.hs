53c0108e91fec1ff6dc56e6634367516289c5421
川路慎一 <bowmoq@gmail.com>
2019-09-06 06:17:54 +0900

    add ch12 exercise 12.1

---
+++ ch12/exercise.hs
@@ -0,0 +1,10 @@
+-- exercise 12.1
+data Tree a = Leaf | Node (Tree a) a (Tree a)
+              deriving Show
+
+instance Functor Tree where
+  -- fmap :: (a -> b) -> Tree a -> Tree b
+  fmap f Leaf = Leaf
+  fmap f (Node l a r) = Node (fmap f l) (f a) (fmap f r)
+
+testTree = Node (Node Leaf 10 Leaf) 20 (Leaf)
