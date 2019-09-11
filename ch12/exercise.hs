31a0f1e97f68a2e046ac0302e989612511f6bfed
川路慎一 <bowmoq@gmail.com>
2019-09-12 06:15:15 +0900

    add exercise 12.4

--- ch12/exercise.hs
+++ ch12/exercise.hs
@@ -18,3 +18,16 @@ testTree = Node (Node Leaf 10 Leaf) 20 (Leaf)
 --instance Applicative ((->) a) where
 --  pure x = (\_ -> x)
 --  f <*> g = \x -> f x (g x)
+
+-- exercise 12.4
+newtype ZipList' a = Z' [a] deriving Show
+instance Functor ZipList' where
+  -- fmap :: (a -> b) -> ZipList' a -> ZipList' b
+  fmap g (Z' xs) = Z' (fmap g xs)
+
+instance Applicative ZipList' where
+  -- pure :: a -> ZipList' a
+  pure x = Z' (repeat x)
+
+  -- (<*>) :: ZipList' (a -> b) -> ZipList' a -> ZipList' b
+  (Z' gs) <*> (Z' xs) = Z' [g x | (g, x) <- zip gs xs]
