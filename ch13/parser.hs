499d8724136c90be7bd67831ec10884e6d7ada4e
川路慎一 <bowmoq@gmail.com>
2019-09-16 08:16:50 +0900

    add calculator

--- ch13/parser.hs
+++ ch13/parser.hs
@@ -1,5 +1,6 @@
 import Control.Applicative
 import Data.Char
+import System.IO
 
 newtype Parser a = P (String -> [(a, String)])
 
@@ -139,8 +140,98 @@ factor = do symbol "("
             return e
           <|> natural
 
-eval :: String -> Int          
-eval xs = case (parse expr xs) of
-            [(n, [])]  -> n
-            [(_, out)] -> error ("Unused input " ++ out)
-            []         -> error "Invalid input"
+-- eval :: String -> Int          
+-- eval xs = case (parse expr xs) of
+--             [(n, [])]  -> n
+--             [(_, out)] -> error ("Unused input " ++ out)
+--             []         -> error "Invalid input"
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int, Int)
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do goto p
+                  putStr xs
+
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H") 
+
+getCh :: IO Char
+getCh = do hSetEcho stdin False
+           x <- getChar
+           hSetEcho stdin True
+           return x
+
+box :: [String]
+box = [
+  "+---------------+",
+  "|               |",
+  "+---+---+---+---+",
+  "| q | c | d | = |",
+  "+---+---+---+---+",
+  "| 1 | 2 | 3 | + |",
+  "+---+---+---+---+",
+  "| 4 | 5 | 6 | - |",
+  "+---+---+---+---+",
+  "| 7 | 8 | 9 | * |",
+  "+---+---+---+---+",
+  "| 0 | ( | ) | / |",
+  "+---+---+---+---+"]
+
+buttons :: String
+buttons = standard ++ extra
+          where
+            standard = "qcd=123+456-789*0()/"
+            extra    = "QCD \ESC\BS\DEL\n"
+
+showbox :: IO()
+showbox = sequence_ [writeat (1,y) b | (y,b) <- zip [1..] box]
+
+display :: String -> IO()
+display xs = do writeat (3,2) (replicate 13 ' ')
+                writeat (3,2) (reverse (take 13 (reverse xs)))
+
+calc :: String -> IO()
+calc xs = do display xs
+             c <- getCh
+             if elem c buttons then
+                process c xs
+             else
+                do beep 
+                   calc xs
+
+beep :: IO ()
+beep = putStr "\BEL"
+
+process :: Char -> String -> IO ()
+process c xs | elem c "qQ\ESC"    = quit
+             | elem c "dD\BS\DEL" = delete xs
+             | elem c "=\n"       = eval xs
+             | elem c "cC"        = clear
+             | otherwise          = press c xs
+
+quit :: IO ()
+quit = goto (1,14)
+
+delete :: String -> IO ()
+delete [] = calc []
+delete xs = calc (init xs)
+
+eval :: String -> IO ()
+eval xs = case parse expr xs of
+              [(n,[])] -> calc (show n)
+              _        -> do beep
+                             calc xs
+
+clear :: IO ()
+clear = calc []
+
+press :: Char -> String -> IO ()
+press c xs = calc (xs ++ [c])
+
+run :: IO ()
+run = do cls
+         showbox
+         clear
