3c8cc0551cb279e05d1b183b7d092807e7d6fefe
川路慎一 <bowmoq@gmail.com>
2019-09-15 12:46:55 +0900

    parser ~13.7

--- ch13/parser.hs
+++ ch13/parser.hs
@@ -36,4 +36,84 @@ three = do x <- item
            item
            z <- item
            return (x, z)
-        
+
+instance Alternative Parser where           
+  -- empty :: Parser a
+  empty = P (\inp -> [])
+  -- (<|>) :: Parser a -> Parser a -> Parser a
+  p <|> q = P (\inp -> case parse p inp of
+                        [] -> parse q inp
+                        [(v, out)] -> [(v, out)])
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do x<- item
+           if p x then return x else empty
+  
+digit :: Parser Char           
+digit = sat isDigit
+
+lower :: Parser Char
+lower = sat isLower
+
+upper :: Parser Char
+upper = sat isUpper
+
+letter :: Parser Char
+letter = sat isAlpha
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+string :: String -> Parser String
+string [] = return []
+string (x:xs) = do char x
+                   string xs
+                   return (x:xs)
+  
+ident :: Parser String
+ident = do x <- lower
+           xs <- many alphanum
+           return (x:xs)
+
+nat :: Parser Int
+nat = do xs <- some digit
+         return (read xs)
+
+space :: Parser ()
+space = do many (sat isSpace)
+           return ()
+
+int :: Parser Int
+int = do char '-'
+         n <- nat 
+         return (-n)
+       <|> nat
+
+token :: Parser a -> Parser a
+token p = do space
+             v <- p
+             space
+             return v
+
+identifier :: Parser String
+identifier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+integer :: Parser Int
+integer = token int
+
+symbol :: String -> Parser String
+symbol xs = token (string xs)
+
+nats :: Parser [Int]
+nats = do symbol "["
+          n <- natural
+          ns <- many (do symbol ","
+                         natural)
+          symbol "]"
+          return (n:ns)
