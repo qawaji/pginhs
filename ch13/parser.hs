3fa7a65b730bd6e668d4a8b6c6e7cc18e82a5955
川路慎一 <bowmoq@gmail.com>
2019-09-15 13:03:24 +0900

    parser ~eval

--- ch13/parser.hs
+++ ch13/parser.hs
@@ -117,3 +117,30 @@ nats = do symbol "["
                          natural)
           symbol "]"
           return (n:ns)
+
+expr :: Parser Int
+expr = do t <- term
+          do symbol "+"
+             e <- expr
+             return (t + e)
+           <|> return t
+
+term :: Parser Int
+term = do f <- factor
+          do symbol "*"
+             t <- term
+             return (f * t)
+           <|> return f
+
+factor :: Parser Int
+factor = do symbol "("
+            e <- expr
+            symbol ")"
+            return e
+          <|> natural
+
+eval :: String -> Int          
+eval xs = case (parse expr xs) of
+            [(n, [])]  -> n
+            [(_, out)] -> error ("Unused input " ++ out)
+            []         -> error "Invalid input"
