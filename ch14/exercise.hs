a93adae526be2cb5200bc08663dfc516980eeb19
川路慎一 <bowmoq@gmail.com>
2019-09-20 05:23:12 +0900

    ex 14.2

--- ch14/exercise.hs
+++ ch14/exercise.hs
@@ -3,3 +3,11 @@
 --instance (Monoid a, Monoid b) => Monoid (a, b) where
 --  mempty = (mempty, mempty)
 --  (x1,y1) `mappend` (x2,y2) = (x1 `mappend` x2, y1 `mappend` y2)
+
+-- exercise 14.2
+-- コンパイルするとDuplicate instance declarations
+--instance Monoid b => Monoid (a -> b) where
+--  -- mempty :: (a -> b)
+--  mempty = \_ -> mempty
+--  -- mappend :: (a -> b) -> (a -> b) -> (a -> b)
+--  f `mappend` g = \x -> f x `mappend` g x
