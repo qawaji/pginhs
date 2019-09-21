62992bf6040ade9097b652d4b9f4d115d6f31bdc
川路慎一 <bowmoq@gmail.com>
2019-09-21 13:11:45 +0900

    14.3 Foldable

--- ch14/exercise.hs
+++ ch14/exercise.hs
@@ -1,3 +1,6 @@
+import Data.Foldable
+import Data.Monoid
+
 -- exercise 14.1
 -- コンパイルするとDuplicate instance declarations
 --instance (Monoid a, Monoid b) => Monoid (a, b) where
@@ -11,3 +14,23 @@
 --  mempty = \_ -> mempty
 --  -- mappend :: (a -> b) -> (a -> b) -> (a -> b)
 --  f `mappend` g = \x -> f x `mappend` g x
+
+data Maybe' a = Nothing' | Just' a
+  deriving (Show, Ord, Eq, Read)
+
+instance Foldable Maybe' where
+  -- fold :: Monoid a => (Maybe a) -> a
+  fold Nothing' = mempty
+  fold (Just' x) = x
+
+  -- foldMap :: Monoid a => (a -> b) -> Maybe a -> b
+  foldMap _ Nothing' = mempty
+  foldMap f (Just' x) = f x
+
+  -- foldr :: (a -> b -> b) -> b -> Maybe a -> b
+  foldr _ z Nothing' = z
+  foldr f z (Just' x) = f x z
+
+  -- foldl :: (a -> b -> a) -> a -> Maybe b -> a
+  foldl _ z Nothing' = z
+  foldl f z (Just' x) = f z x
