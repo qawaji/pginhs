78ddda1f297032ad806c17eade23d0140a533a5d
川路慎一 <bowmoq@gmail.com>
2019-09-23 08:17:53 +0900

    exercise 15.5

--- ch15/exercise.hs
+++ ch15/exercise.hs
@@ -2,6 +2,21 @@
 fibs :: [Integer]
 fibs = 0:1:[x1 + x2 | (x1,x2) <- zip fibs (tail fibs)]
 
+-- 15.5
+data Tree a = Leaf | Node (Tree a) a (Tree a)
+              deriving Show
+
+repeat' :: a -> Tree a
+repeat' x = xs where xs = Node (repeat' x) x (repeat' x)
+
+take' :: Int -> Tree a -> Tree a
+take' 0 _            = Leaf
+take' _ Leaf         = Leaf
+take' n (Node l x r) = Node (take' (n - 1) l) x (take' (n - 1) r)
+
+replicate' :: Int -> a -> Tree a
+replicate' n = take' n . repeat'
+
 -- 15.6
 sqroot :: Double -> Double
 sqroot n = v
