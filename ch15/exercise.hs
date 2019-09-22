42a1df69f4f9ca27026fefaa762d867af0a515d0
川路慎一 <bowmoq@gmail.com>
2019-09-22 20:24:34 +0900

    exercise 15.6

--- ch15/exercise.hs
+++ ch15/exercise.hs
@@ -2,3 +2,10 @@
 fibs :: [Integer]
 fibs = 0:1:[x1 + x2 | (x1,x2) <- zip fibs (tail fibs)]
 
+-- 15.6
+sqroot :: Double -> Double
+sqroot n = v
+  where
+    next a = (a + n/a) / 2
+    iter = iterate next 1
+    (v, _) = head $ dropWhile (\(x, y) -> abs (x - y) > 0.00001 )[(x, y) | (x, y) <- zip iter (tail iter)]
